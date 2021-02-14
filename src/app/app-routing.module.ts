aa907f6009b19c7df11a49b0458c4e3218a41c55
sumansharma01 <suman.sharma0331@gmail.com>
2021-02-15 00:56:27 +0530

    added routes of the navs sir asked us to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimationsComponent } from './animations/animations.component';
+import { AppComponent } from './app.component';
+import { BordersComponent } from './borders/borders.component';
+import { ColorsComponent } from './colors/colors.component';
+import { OtherComponent } from './other/other.component';
+import { TablesComponent } from './tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'border',
+    component:BordersComponent
+  },
+  {
+    path:'color',
+    component:ColorsComponent
+  },
+  {
+    path:'other',
+    component:OtherComponent
+  },
+  {
+    path:'animation',
+    component:AnimationsComponent
+  },
+  {
+    path:'table',
+    component:TablesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { UppercardsComponent } from './uppercards/uppercards.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BelownavbarComponent } from './belownavbar/belownavbar.component';
+import { ColorsComponent } from './colors/colors.component';
+import { BordersComponent } from './borders/borders.component';
+import { AnimationsComponent } from './animations/animations.component';
+import { OtherComponent } from './other/other.component';
+import { TablesComponent } from './tables/tables.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { BelownavbarComponent } from './belownavbar/belownavbar.component';
     UppercardsComponent,
     NavbarComponent,
     SidebarComponent,
-    BelownavbarComponent
+    BelownavbarComponent,
+    ColorsComponent,
+    BordersComponent,
+    AnimationsComponent,
+    OtherComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
